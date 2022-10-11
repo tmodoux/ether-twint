be37eca2fef3cbf33bfdbdc645d3328c6e31b8d9
tmodoux <d_news00@hotmail.com>
2022-10-11 22:26:17 +0200

    use hashrouter for gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 import Pay from "./components/pay";
 
